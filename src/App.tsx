42ba86841f28e8dc97eec2a6452bf4e5010ccfb4
Daniel Dunbar <dunbardanielj@gmail.com>
2021-08-12 22:09:40 -0700

    Add login screen and absolute paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Main from 'src/main/Main';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h6">HealthyMynd</Typography>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+      <Main></Main>
     </div>
   );
 }
---
+++ src/main/Main.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Login from 'src/main/login/Login';
+
+function Main() {
+  return <Login></Login>;
+}
+
+export default Main;
